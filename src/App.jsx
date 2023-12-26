89d2c71ca5d258c2689739cfef51daf95eddb36e
Hassan Farooq <hfarooq314@gmail.com>
2023-12-26 16:22:26 +0500

    create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
+import Header from "./component/Header";
+
 export default function App() {
-  return <h1 className="text-red-500"> App</h1>;
+  return (
+    <BrowserRouter>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+import Header from "../component/Header";
+
+export default function Home() {
+  return <div></div>;
+}
