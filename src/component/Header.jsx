7c013af8a43bf3934ad33d54b67017a33aa45ece
Hassan Farooq <hfarooq314@gmail.com>
2023-12-29 19:45:14 +0500

    complete sign up page UI

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -6,12 +6,12 @@ export default function Header() {
     <header className="bg-slate-200 shadow-md">
       <div className="justify-between flex items-center max-w-6xl mx-auto p-3">
         <Link to={"/"}>
-          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap  ">
             <span className="text-slate-500">Sahand</span>
             <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center ">
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center  ">
           <input
             type="text"
             placeholder="serach"
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,3 +1,44 @@
+import { Link } from "react-router-dom";
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold m-7">Sign-Up</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          className="border p-3 rounded-lg"
+          placeholder="Username"
+          type="text"
+          id="username"
+        />
+        <input
+          className="border p-3 rounded-lg"
+          placeholder="Email"
+          type="email"
+          id="email"
+        />
+        <input
+          className="border p-3 rounded-lg"
+          placeholder="Password"
+          type="password"
+          id="password"
+        />
+        <button
+          className="bg-slate-700 text-white p-3 rounded-lg
+           uppercase hover:opacity-90 disabled:opacity-80"
+          type="submit"
+        >
+          Sign-Up
+        </button>
+      </form>
+      <p className="flex gap-2 mt-5">
+        Already have a account?
+        <Link
+          className="text-blue-700 font-bold hover:opacity-95"
+          to={"/sign-In"}
+        >
+          Sign-In
+        </Link>
+      </p>
+    </div>
+  );
 }
