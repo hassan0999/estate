9f1f63ff4c9711a14d713d6b48cdbf9ea77b26a0
Hassan Farooq <hfarooq314@gmail.com>
2023-12-31 18:17:38 +0500

    add redux toolkit

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,3 +1,86 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+
 export default function SignIn() {
-  return <div>SignIn</div>;
+  const nav = useNavigate();
+  const dispatch = useDispatch();
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(signInStart());
+      const response = await fetch("server/signIn", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      console.log(data);
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+        return;
+      }
+      dispatch(signInSuccess(data));
+      nav("/");
+    } catch (error) {
+      dispatch(signInFailure(error.message));
+    }
+  };
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold m-7">Sign-In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          className="border p-3 rounded-lg"
+          placeholder="Username"
+          type="text"
+          id="username"
+          onChange={handleChange}
+        />
+
+        <input
+          className="border p-3 rounded-lg"
+          placeholder="Password"
+          type="password"
+          id="password"
+          onChange={handleChange}
+        />
+        <button
+          className="bg-slate-700 text-white p-3 rounded-lg
+           uppercase hover:opacity-90 disabled:opacity-80"
+          type="submit"
+        >
+          {loading ? "Loading..." : "Sign In"}
+        </button>
+      </form>
+      <div>
+        <p className="flex gap-2 mt-5">Do not have a account?</p>
+        <Link
+          className="text-blue-700 font-bold hover:opacity-95"
+          to={"/Sign-up"}
+        >
+          Sign-Up
+        </Link>
+      </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
+    </div>
+  );
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,26 +1,54 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 export default function SignUp() {
+  const nav = useNavigate();
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("server/signUp", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await response.json();
+    console.log(data);
+    nav("/Sign-in");
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold m-7">Sign-Up</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           className="border p-3 rounded-lg"
           placeholder="Username"
           type="text"
           id="username"
+          onChange={handleChange}
         />
         <input
           className="border p-3 rounded-lg"
           placeholder="Email"
           type="email"
           id="email"
+          onChange={handleChange}
         />
         <input
           className="border p-3 rounded-lg"
           placeholder="Password"
           type="password"
           id="password"
+          onChange={handleChange}
         />
         <button
           className="bg-slate-700 text-white p-3 rounded-lg
